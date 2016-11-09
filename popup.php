136d7b235ddf90b85516f982cb7d47e8feca7761
marcelkrumbel <marceltje5@gmail.com>
2016-11-10 00:12:48 +0100

    popup
    aangepast detail

--- popup.php
+++ popup.php
@@ -11,7 +11,6 @@
 <body>
 
 <div class="container">
-  <h2>Modal Example</h2>
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ID + Percentage competenties </button>
 
