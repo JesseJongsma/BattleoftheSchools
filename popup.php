ebeb3f879c1409c32d337d608c12283e68f35801
marcelkrumbel <marceltje5@gmail.com>
2016-11-10 02:47:57 +0100

    klaar popup
    hahahah

--- popup.php
+++ popup.php
@@ -1,49 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Title</title>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <script src="js/jquery-1.11.2.min.js"></script>
-    <script src="js/battle.js"></script>
-
-</head>
-<body>
-    <div class="menu-wrapper">  
-      <div class="navbar-top">
-        <div class="toggle-btn-holder">
-          <div class="toggle-btn">
-          </div>
-        </div>
-        <div class="header-text-holder">
-          <div class="header-text">
-            Hier je titel!
-          </div>
-        </div>
-      </div>
-
-      <?php include 'menu.php' ;?>
-    
-    </div>
-    <div class='margin-top'></div>
-    <div class="container-fluid">
-        <div class="col-sm-2"></div>
-
-        <div class="col-sm-8">
-        <!-- Content -->
         <!-- Trigger the modal with a button -->
         <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ID + Percentage competenties </button>
 
@@ -70,17 +24,3 @@
             	</div>
             	
             	<button style="margin:20px" type="submit" class="btn btn-default">Submit</button>
-
-            </div>
-            
-          </div>
-        </div>
-        </div>
-
-        <div class="col-sm-2"></div>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
