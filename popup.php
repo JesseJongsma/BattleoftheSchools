24ac11d5f56daec38b644a02c7bbdfeb548cebff
marcelkrumbel <marceltje5@gmail.com>
2016-11-10 01:07:33 +0100

    popup puur
    alleen de code van de pop up

--- popup.php
+++ popup.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ID + Percentage competenties </button>
 
@@ -40,8 +27,3 @@
       
     </div>
   </div>
-  
-</div>
-
-</body>
-</html>
