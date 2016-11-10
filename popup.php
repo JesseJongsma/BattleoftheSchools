3b61897744dbdf1cc3ee6d4d6ed4b145f2effe15
marcelkrumbel <marceltje5@gmail.com>
2016-11-10 01:43:25 +0100

    pop up gefixt
    heb in het sjabloon ook even een link toegevoegd zodat de popup het doet

--- popup.php
+++ popup.php
@@ -1,31 +1,86 @@
-  <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ID + Percentage competenties </button>
+<!DOCTYPE html>
+<html lang="en">
 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Title</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
-      <!-- Modal content-->
-      <div class="modal-content">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-      	<div style="margin:20px" class="form-group">
-      	  <label>Jou competenties:</label></br>
-      	  <?php
-      	  	echo "competentie 1 ";
-      	  	echo "competentie 2 ";
-      	  	echo "competentie 3 ";
-      	  	echo "competentie 4 ";
-      	  ?>
-      	</div>
+    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="js/battle.js"></script>
 
-      	<div style="margin:20px" class="form-group">
-      	  <label>Vul hier je motivatie in:</label>
-      	  <textarea class="form-control" rows="10" id="comment"></textarea>
-      	</div>
-      	
-      	<button type="submit" class="btn btn-default">Submit</button>
-
+</head>
+<body>
+    <div class="menu-wrapper">  
+      <div class="navbar-top">
+        <div class="toggle-btn-holder">
+          <div class="toggle-btn">
+          </div>
+        </div>
+        <div class="header-text-holder">
+          <div class="header-text">
+            Hier je titel!
+          </div>
+        </div>
       </div>
-      
+
+      <?php include 'menu.php' ;?>
+    
     </div>
-  </div>
+    <div class='margin-top'></div>
+    <div class="container-fluid">
+        <div class="col-sm-2"></div>
+
+        <div class="col-sm-8">
+        <!-- Content -->
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ID + Percentage competenties </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+          <div class="modal-dialog">
+          
+            <!-- Modal content-->
+            <div class="modal-content">
+
+            	<div style="margin:20px" class="form-group">
+            	  <label>Jou competenties:</label></br>
+            	  <?php
+            	  	echo "competentie 1 ";
+            	  	echo "competentie 2 ";
+            	  	echo "competentie 3 ";
+            	  	echo "competentie 4 ";
+            	  ?>
+            	</div>
+
+            	<div style="margin:20px" class="form-group">
+            	  <label>Vul hier je motivatie in:</label>
+            	  <textarea class="form-control" rows="10" id="comment"></textarea>
+            	</div>
+            	
+            	<button style="margin:20px" type="submit" class="btn btn-default">Submit</button>
+
+            </div>
+            
+          </div>
+        </div>
+        </div>
+
+        <div class="col-sm-2"></div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
--- sjabloon.php
+++ sjabloon.php
@@ -14,8 +14,12 @@
     <link href="css/style.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
     <script src="js/jquery-1.11.2.min.js"></script>
     <script src="js/battle.js"></script>
+
 
 </head>
 <body>
